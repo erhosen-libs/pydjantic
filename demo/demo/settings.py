36dddc1d9f498da49e11e3001e28455ce19ead31
Andrey Lepekhin <andrey@lepekhin.com>
2023-11-27 19:54:00 +0300

    Merged latest master and fixed lint

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from typing import Dict, List
 
-from pydantic import BaseSettings, Field
-from pydantic.fields import Undefined
+from pydantic import Field
+from pydantic.v1.fields import Undefined
+from pydantic_settings import BaseSettings
 
 from pydjantic import BaseDBConfig, to_django
 
@@ -26,62 +27,67 @@ BASE_DIR = CUR_DIR.parent
 class DatabaseSettings(BaseDBConfig):
     # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
     default: str = Field(
-        default=str(f'sqlite:///{BASE_DIR}/db.sqlite3'), env="DATABASE_URL", conn_max_age=0, ssl_require=False
+        default=str(f"sqlite:///{BASE_DIR}/db.sqlite3"),
+        alias="DATABASE_URL",
+        conn_max_age=0,
+        ssl_require=False,
     )
 
     class Config:
-        env_file = CUR_DIR / '.env'
+        env_file = CUR_DIR / ".env"
 
 
 class GeneralSettings(BaseSettings):
     # https://docs.djangoproject.com/en/dev/ref/settings/
-    SECRET_KEY: str = Field(default=Undefined, env='DJANGO_SECRET_KEY')
-    DEBUG: bool = Field(default=False, env='DEBUG')
+    SECRET_KEY: str = Field(default=Undefined, alias="DJANGO_SECRET_KEY")
+    DEBUG: bool = Field(
+        default=False,
+    )
     DATABASES: DatabaseSettings = DatabaseSettings()
 
     ALLOWED_HOSTS: List[str] = []
-    ROOT_URLCONF: str = 'demo.urls'
-    WSGI_APPLICATION: str = 'demo.wsgi.application'
+    ROOT_URLCONF: str = "demo.urls"
+    WSGI_APPLICATION: str = "demo.wsgi.application"
 
     INSTALLED_APPS: List[str] = [
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-        'django.contrib.staticfiles',
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
     ]
 
     MIDDLEWARE: List[str] = [
-        'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        "django.middleware.security.SecurityMiddleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
     ]
 
     AUTH_PASSWORD_VALIDATORS: List[Dict] = [
         {
-            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
         },
         {
-            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
         },
         {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+            "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
         },
         {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+            "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
         },
     ]
 
 
 class I18NSettings(BaseSettings):
     # https://docs.djangoproject.com/en/3.1/topics/i18n/
-    LANGUAGE_CODE: str = 'en-us'
-    TIME_ZONE: str = 'UTC'
+    LANGUAGE_CODE: str = "en-us"
+    TIME_ZONE: str = "UTC"
     USE_I18N: bool = True
     USE_L10N: bool = True
     USE_TZ: bool = True
@@ -89,19 +95,19 @@ class I18NSettings(BaseSettings):
 
 class StaticSettings(BaseSettings):
     # https://docs.djangoproject.com/en/3.1/howto/static-files/
-    STATIC_URL: str = '/static/'
+    STATIC_URL: str = "/static/"
 
     TEMPLATES: List[Dict] = [
         {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [],
-            'APP_DIRS': True,
-            'OPTIONS': {
-                'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages',
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [],
+            "APP_DIRS": True,
+            "OPTIONS": {
+                "context_processors": [
+                    "django.template.context_processors.debug",
+                    "django.template.context_processors.request",
+                    "django.contrib.auth.context_processors.auth",
+                    "django.contrib.messages.context_processors.messages",
                 ],
             },
         },
@@ -110,7 +116,7 @@ class StaticSettings(BaseSettings):
 
 class ProjectSettings(GeneralSettings, I18NSettings, StaticSettings):
     class Config:
-        env_file = CUR_DIR / '.env'
+        env_file = CUR_DIR / ".env"
 
 
 to_django(ProjectSettings())
